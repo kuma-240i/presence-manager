3804627c966e06cab779e82cada8c142bcbcd408
kuma-240i <kumano.kobe@gmail.com>
2021-11-12 06:16:21 +0900

    勤務地表示

--- public/js/api.js
+++ public/js/api.js
@@ -11,9 +11,9 @@ function exec() {
             return person.presence !== 'テレワーク中' && person.presence !== '業務終了';
         }).reduce((acc, cur) => {
             if (cur.message === null){
-                return acc + cur.name + "　　投稿時間：" + cur.update_time.slice(0, 4) + "/" + cur.update_time.slice(5, 7) + "/" + cur.update_time.slice(8, 10) + " " + cur.update_time.slice(11, 13) + ":" + cur.update_time.slice(14, 16) + "<br>";
+                return acc + cur.name + "　　" + cur.presence + "　投稿時間：" + cur.update_time.slice(0, 4) + "/" + cur.update_time.slice(5, 7) + "/" + cur.update_time.slice(8, 10) + " " + cur.update_time.slice(11, 13) + ":" + cur.update_time.slice(14, 16) + "<br>";
             }else{
-                return acc + cur.name + "　　投稿時間：" + cur.update_time.slice(0, 4) + "/" + cur.update_time.slice(5, 7) + "/" + cur.update_time.slice(8, 10) + " " + cur.update_time.slice(11, 13) + ":" + cur.update_time.slice(14, 16) + " 「" + cur.message + "」<br>";
+                return acc + cur.name + "　　" + cur.presence + "　投稿時間：" + cur.update_time.slice(0, 4) + "/" + cur.update_time.slice(5, 7) + "/" + cur.update_time.slice(8, 10) + " " + cur.update_time.slice(11, 13) + ":" + cur.update_time.slice(14, 16) + " 「" + cur.message + "」<br>";
             }
         },'');
         
