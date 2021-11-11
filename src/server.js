f41dda56010ef0218df028ad88ad823d5caee529
kuma-240i <kumano.kobe@gmail.com>
2021-11-12 07:25:26 +0900

    Access-Control-Allow-Origin追加

--- src/server.js
+++ src/server.js
@@ -8,6 +8,19 @@ const setupExpressServer = () => {
 
   app.use(express.static('public'));
 
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET,PUT,POST,DELETE,OPTIONS,PATCH"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, authorization"
+    );
+    next();
+  });
+
   app.get('/v1/presences', (req, res) => {
     knex.select('*').from('presence').then((rows) =>{
       res.send(rows);
